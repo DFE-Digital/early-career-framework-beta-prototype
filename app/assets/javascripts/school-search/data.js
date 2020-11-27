643a8f657dbbe571e7222fa4de97b471572b9f45
Ethan Mills <ethan.mills@softwire.com>
2020-11-27 16:04:31 +0000

    Add school search page

---
+++ app/assets/javascripts/school-search/data.js
@@ -0,0 +1,97 @@
+export const allSchools = [
+  {
+    urn: 123456,
+    name: 'Corfe Hills School',
+    address: 'Higher Blandford Road, Broadstone, Dorset, BH18 9BG',
+    phase: 'secondary',
+    phaseDisplay: 'Secondary',
+    type: 'academy',
+    typeDisplay: 'Academy',
+    pupilPremium: false,
+    rural: false,
+    alreadyPartnered: false,
+    partnershipConfirmed: false
+  },
+  {
+    urn: 123456,
+    name: 'Rural school',
+    address: 'Higher Blandford Road, Broadstone, Dorset, BH18 9BG',
+    phase: 'secondary',
+    phaseDisplay: 'Secondary',
+    type: 'academy',
+    typeDisplay: 'Academy',
+    pupilPremium: false,
+    rural: true,
+    alreadyPartnered: false,
+    partnershipConfirmed: false
+  },
+  {
+    urn: 123456,
+    name: 'Pupil Premium School',
+    address: 'Higher Blandford Road, Broadstone, Dorset, BH18 9BG',
+    phase: 'secondary',
+    phaseDisplay: 'Secondary',
+    type: 'academy',
+    typeDisplay: 'Academy',
+    pupilPremium: true,
+    rural: false,
+    alreadyPartnered: false,
+    partnershipConfirmed: false
+  },
+  {
+    urn: 123456,
+    name: 'Rural PP School',
+    address: 'Higher Blandford Road, Broadstone, Dorset, BH18 9BG',
+    phase: 'secondary',
+    phaseDisplay: 'Secondary',
+    type: 'academy',
+    typeDisplay: 'Academy',
+    pupilPremium: true,
+    rural: true,
+    alreadyPartnered: false,
+    partnershipConfirmed: true
+  },
+  {
+    urn: 123456,
+    name: 'Corfe Hills School',
+    address: 'Higher Blandford Road, Broadstone, Dorset, BH18 9BG',
+    phase: 'secondary',
+    phaseDisplay: 'Secondary',
+    type: 'academy',
+    typeDisplay: 'Academy',
+    pupilPremium: false,
+    rural: false,
+    alreadyPartnered: true,
+    partnershipConfirmed: false
+  },
+  {
+    urn: 123456,
+    name: 'School With A Very Very Very Very Very Very Very Long Name',
+    address: 'Higher Blandford Road, Broadstone, Dorset, BH18 9BG',
+    phase: 'secondary',
+    phaseDisplay: 'Secondary',
+    type: 'academy',
+    typeDisplay: 'Academy',
+    pupilPremium: false,
+    rural: false,
+    alreadyPartnered: false,
+    partnershipConfirmed: true
+  }
+]
+
+export const allMats = [
+  {
+    name: 'MAT1',
+    id: 'TR1234',
+    uid: 123456,
+    members: 5,
+    partnered: false
+  },
+  {
+    name: 'MAT2',
+    id: 'TR1234',
+    uid: 123456,
+    members: 5,
+    partnered: true
+  }
+]
---
+++ app/assets/javascripts/school-search/school-search.js
@@ -0,0 +1,78 @@
+import { allSchools } from './data.js'
+
+const prevOnload = window.onload
+window.onload = () => {
+  setupEventListeners()
+  applyFiltering()
+  prevOnload && prevOnload()
+}
+
+// function that takes a list of schools and filters a list of them based on pupil premium
+function setupEventListeners () {
+  const applyFiltersButtons = document.getElementsByName('apply-school-filters')[0]
+  applyFiltersButtons.addEventListener('click', applyFiltering)
+}
+
+function applyFiltering () {
+  const resultsHtml = window.nunjucks.render('school-results.html', { schools: filteredSchools() })
+  document.getElementById('school-results').innerHTML = resultsHtml
+}
+
+function filteredSchools () {
+  let schools = allSchools
+  if (getName().length > 0) {
+    schools = filterName(schools, getName())
+  }
+  if (getCharacteristics().length > 0) {
+    schools = filterCharacteristics(schools, getCharacteristics())
+  }
+
+  if (getPhases().length > 0) {
+    schools = filterPhase(schools, getPhases())
+  }
+  if (getTypes().length > 0) {
+    schools = filterType(schools, getTypes())
+  }
+
+  return schools
+}
+
+function getName () {
+  return document.getElementById('school-name').value
+}
+
+function getCharacteristics () {
+  return checkedBoxes('characteristic')
+}
+
+function getPhases () {
+  return checkedBoxes('phase')
+}
+
+function getTypes () {
+  return checkedBoxes('type')
+}
+
+function checkedBoxes (name) {
+  return Array.from(document.getElementsByName(name)).filter(box => box.checked).map(box => box.value)
+}
+
+function filterName (schools, name) {
+  return schools.filter((school) => {
+    return school.name.toLowerCase().indexOf(name.toLowerCase()) !== -1
+  })
+}
+
+function filterCharacteristics (schools, characteristics) {
+  const showRural = characteristics.includes('rural')
+  const showPupilPremium = characteristics.includes('pupil-premium')
+  return schools.filter(school => (showRural && school.rural) || (showPupilPremium && school.pupilPremium))
+}
+
+function filterPhase (schools, phase) {
+  return schools.filter((school) => phase.includes(school.phase))
+}
+
+function filterType (schools, type) {
+  return schools.filter((school) => type.includes(school.type))
+}
