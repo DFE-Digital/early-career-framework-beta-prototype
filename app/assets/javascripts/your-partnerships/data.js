8bb855afe7937f5cca3e77798a94b39a76c581be
Dominique <dominique.sherwood@gmail.com>
2020-12-01 21:05:14 +0000

    Edits to error page, success edits (10 schools)

--- app/assets/javascripts/your-partnerships/data.js
+++ app/assets/javascripts/your-partnerships/data.js
@@ -1,5 +1,17 @@
 export const partneredSchools = [
   {
+    name: 'Corfe Hills School',
+    urn: '123456',
+    partnershipStatus: 'pending',
+  },
+
+  {
+    name: 'Harris Academy Tottenham',
+    urn: '140935',
+    partnershipStatus: 'pending',
+  },
+
+  {
     name: "Westminster City School",
     urn: "138312",
     partnershipStatus: "confirmed"
